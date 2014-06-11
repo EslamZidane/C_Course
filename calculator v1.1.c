6992442e1a35b38997c8b8b738bd18c24eba4c3d
EslamZidane <eslamfouad@hotmail.com>
2014-06-11 10:25:10 +0300

    Create calculator v1.1.c
    Lecture 5

---
+++ calculator v1.1.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+ 
+double add(double a, double b);
+double sub(double a, double b);
+double divid(double a, double b);
+double product(double a, double b);
+typedef double (*p2fun)(double, double) ;
+p2fun op(char op_char);
+double (*op(char))(double, double);
+ 
+void main(int argc, char *argv[])
+{
+    char op_code = argv[3][0];
+    double (*temp)(double , double) = NULL;
+ 
+    temp = op(argv[2][0]);
+    printf("%lf %lf\n" , atof(argv[1]) , atof(argv[3]));
+    printf("Result: %lf\n", temp(atof(argv[1]) , atof(argv[3])));
+    return;
+}
+ 
+p2fun op(char op_char)
+{  
+    switch(op_char)
+    {
+      case '+':
+        return add;
+        break;
+      case '-':
+        return sub;
+      case '/':
+        return divid;
+      case '*':
+        return product;
+      default:;
+    }
+}
+ 
+double add(double a, double b)
+{
+    return a+b;
+}
+ 
+double sub(double a, double b)
+{
+    return a-b;
+}
+ 
+double divid(double a, double b)
+{
+    return a/b;
+}
+ 
+double product(double a, double b)
+{
+    return a*b;
+}
