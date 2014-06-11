49089ae60d2411ae049118d414e7a05a111c96ac
EslamZidane <eslamfouad@hotmail.com>
2014-06-11 10:14:04 +0300

    Create DTypes_Size.c
    Lecture 2

---
+++ DTypes_Size.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+  
+void main(void)
+{
+  
+printf(" Size of Char is %d \n Size of Int is %d \n Size of Float is %d \n Size of Double is %d \n Size of Long Long is %d \n Size of Long Double is %d \n Size of Wide Char is %d \n" , sizeof(char), sizeof(int), sizeof(float), sizeof(double), sizeof(long long), sizeof(long double), sizeof(wchar_t));
+return;
+} 
